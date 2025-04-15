dd729f52394ecfe811f4af77537adb97500f7165
Shakir Sultanov <sultanovshakir12@gmail.com>
2025-04-15 03:12:06 +0200

    Styles fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,49 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import './index.css'
 
-import { Layout } from "@/pages/Layout";
-import { ChatHistory } from "@/pages/ChatHistory";
-import Analytics from "@/pages/Analytics";
-import Questions from "@/pages/Questions";
-
-const router = createBrowserRouter([
-  {
-    path: "/admin-panel",
-    element: <Layout />,
-    children: [
-      {
-        path: "/admin-panel/questions",
-        element: <Questions />,
-      },
-      {
-        path: "/admin-panel/analytics",
-        element: <Analytics />,
-      },
-      {
-        path: "/admin-panel/chat-history",
-        element: <ChatHistory />,
-      },
-      {
-        path: "/admin-panel",
-        element: <Questions />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Questions />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
